e4db03b25a0acf02004962e468bf82430b7143ca
NaaYaa <denys.tkachuk@tech-stack.io>
2022-01-26 22:25:36 +0200

    provider changed

--- src/services/Provider.js
+++ src/services/Provider.js
@@ -8,28 +8,6 @@ const Provider = (bot, time = 4000) => {
         getRandomData(latitude.min, latitude.max),
         getRandomData(longitude.min, longitude.max))
     return (id) => {
-        if (!!USER_TABLE[id]) {
-            return {
-                unsubscribe: () => {
-                    clearInterval(INTERVAL_ID[id])
-                    delete USER_TABLE[id]
-                }
-            }
-        } else {
-            USER_TABLE[id] = 'subscribed'
-            return {
-                subscribe: () => INTERVAL_ID[id] = setInterval(intervalHandler(id), time),
-            }
-        }
-    }
-}
-const Provider2 = (bot, time = 4000) => {
-    let INTERVAL_ID = {}
-    let USER_TABLE = {}
-    const intervalHandler = (id) => () => bot.sendLocation(id,
-        getRandomData(latitude.min, latitude.max),
-        getRandomData(longitude.min, longitude.max))
-    return (id) => {
         return {
             subscribe: () => {
                 if (USER_TABLE[id]) return
@@ -43,4 +21,4 @@ const Provider2 = (bot, time = 4000) => {
         }
     }
 }
-module.exports = Provider2
+module.exports = Provider
